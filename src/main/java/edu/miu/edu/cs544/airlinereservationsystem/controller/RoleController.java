81a6d0b30a6e6fe62c65cc6e5c8307877e0b1c3e
Daniel Jifar <jifardaniel@gmail.com>
2021-11-14 22:06:20 -0600

    role apis are implemented

--- src/main/java/edu/miu/edu/cs544/airlinereservationsystem/controller/RoleController.java
+++ src/main/java/edu/miu/edu/cs544/airlinereservationsystem/controller/RoleController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/roles")
 public class RoleController {
@@ -17,9 +19,26 @@ public class RoleController {
 
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity<Long> createRole(@Validated @RequestBody Role role) {
-
         Role savedRole = service.saveRole(role);
         return new ResponseEntity<>(savedRole.getId(), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<Role>> getRoles() {
+        List<Role> roles = service.getRoles();
+        return new ResponseEntity<>(roles, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = "application/json")
+    public ResponseEntity<Void> updateRole(@PathVariable Long id, @Validated @RequestBody Role role) {
+        service.updateRole(id, role);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteRole(@PathVariable Long id) {
+        service.deleteRole(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
