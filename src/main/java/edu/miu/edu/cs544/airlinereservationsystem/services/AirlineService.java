44a11d6a57f5a2d90d1f02a8922a9c9b742fa92c
Meron Hadera <merontedros27@gmail.com>
2021-11-14 22:24:48 -0600

    Airline Controller-tested

--- src/main/java/edu/miu/edu/cs544/airlinereservationsystem/services/AirlineService.java
+++ src/main/java/edu/miu/edu/cs544/airlinereservationsystem/services/AirlineService.java
@@ -1,4 +1,14 @@
 package edu.miu.edu.cs544.airlinereservationsystem.services;
 
+import edu.miu.edu.cs544.airlinereservationsystem.database.dto.Airline;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface AirlineService {
+    Airline addAirline(Airline airline);
+    List<Airline> findAll();
+    Optional<Airline> findById(Long id);
+    Airline updateAirline(Long id, Airline airline);
+    void deleteAirline(Long id);
 }
--- src/main/java/edu/miu/edu/cs544/airlinereservationsystem/services/AirlineServiceImpl.java
+++ src/main/java/edu/miu/edu/cs544/airlinereservationsystem/services/AirlineServiceImpl.java
@@ -1,12 +1,46 @@
 package edu.miu.edu.cs544.airlinereservationsystem.services;
 
 import edu.miu.edu.cs544.airlinereservationsystem.database.dao.AirlineRepository;
+import edu.miu.edu.cs544.airlinereservationsystem.database.dto.Airline;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AirlineServiceImpl implements AirlineService {
 
     @Autowired
     AirlineRepository repository;
+
+    @Override
+    public Airline addAirline(Airline airline) {
+        return repository.save(airline);
+    }
+
+    @Override
+    public List<Airline> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Airline> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Airline updateAirline(Long id, Airline airline) {
+        Optional<Airline> entity = repository.findById(id);
+//        entity.get().setId(airline.getId());
+        entity.get().setName(airline.getName());
+        entity.get().setCode(airline.getCode());
+        entity.get().setHistory(airline.getHistory());
+        return repository.save(airline);
+    }
+
+    @Override
+    public void deleteAirline(Long id) {
+        repository.deleteById(id);
+    }
 }
